05bc3eaf0be5566517220a5af2c3cb004a0c9a47
Umer Islam  Najar <najarumer98@gmail.com>
2026-09-05 20:12:39 +0530

    new file

---
+++ oop.py
@@ -0,0 +1,13 @@
+class student:
+    name = "umer"
+    subject = "python"
+    cgpa = 7
+    college = "skuast"
+
+
+
+stu1 = student()
+print(stu1.name , stu1.subject , stu1.cgpa , stu1.college)
+
+
+
